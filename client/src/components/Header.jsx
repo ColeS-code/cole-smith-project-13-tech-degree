7e6ac91a24ee542069fd5ea9a15ca17b87e5e318
Cole <cole.smith.code@gmail.com>
2024-05-15 10:27:00 -0600

    updated header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,11 @@
-const header = () => {
-    return (
-      <h1>Example Header</h1>
-    )
-  }
-  
-  export default header
+< header >
+    <div class="wrap header--flex">
+        <h1 class="header--logo"><a href="index.html">Courses</a></h1>
+        <nav>
+            <ul class="header--signedout">
+                <li><a href="sign-up.html">Sign Up</a></li>
+                <li><a href="sign-in.html">Sign In</a></li>
+            </ul>
+        </nav>
+    </div>
+</header >
