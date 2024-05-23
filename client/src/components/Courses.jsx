a97e4d464aed8351b8c99a7b9df05b2457b5180f
Cole <cole.smith.code@gmail.com>
2024-05-23 12:41:29 -0600

    course stuff

--- client/src/components/Courses.jsx
+++ client/src/components/Courses.jsx
@@ -5,6 +5,17 @@ const fetchData = () => {
     const [coursesData, setCoursesData] = useState([]);
 
     useEffect = () => {
-        
+        const data = async () => {
+            try {
+                const response = await fetch('');
+                if (!response.ok) {
+                    throw new Error ('network not responding')
+                }
+                const jsonData = await response.json()
+                setCoursesData(jsonData);
+            } catch (err) {
+                //error message//
+            }
+        }
     }
 }
